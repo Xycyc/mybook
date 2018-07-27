2439474cba79a4f49d131a6668eb4d7941a66ac7
xyc <victoria135@163.com>
2018-07-27 17:26:12 +0800

    init

---
+++ mybook/src/app/component/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './app.css';
+import Button from 'antd/lib/button';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>ok</div>
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
